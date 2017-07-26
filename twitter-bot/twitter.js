4226fd4b1ac8eabe8e33e2ea87f201f3cc201e23
Josh Hulsey <jhul0388@gmail.com>
2017-07-26 09:28:10 -0500

    Update twitter.js

--- twitter-bot/twitter.js
+++ twitter-bot/twitter.js
@@ -2,72 +2,57 @@ var Twitter = require("twitter");
 var Spotify = require('node-spotify-api');
 var request = require('request');
 
-
 var fs = require('fs');
 
 var command = process.argv[2];
-var commandArgument = process.argv[3];
+var commandA = process.argv[3];
 
 // var commandArgument = process.argv[3];
 ///twitter
 
-var spotify = new Spotify({
-  id: 'cf2e3bdd2f6a4a5792c6409196befee7',
-  secret: '049eaca090384988a0169b470e2c9fa7'
-});
+var spotify = new Spotify({id: 'cf2e3bdd2f6a4a5792c6409196befee7', secret: '049eaca090384988a0169b470e2c9fa7'});
 
-switch(command){
+switch (command) {
   case 'my-tweets':
-  myTweets();
-  break;
+    myTweets();
+    break;
 
   case 'spotify-this-song':
-		mySpotify(commandArgument);
-		break;
+    mySpotify(commandA);
+    mySpotify("Back in black");
+    break;
 
 }
 
 function myTweets() {
 
-var client = new Twitter({
-  consumer_key: 'RwqVXzDjzji6RF85KFWrq65kv',
-  consumer_secret: 'TSKB58nbg8nPw79A9gcjv23e1cGU9vVqsLQs11fM997bZRHbd7',
-  access_token_key: '889721993683238912-PALrUB3r09Q6Pgxp0oWe5hIzgnPqTWV',
-  access_token_secret: 'pMlbEqZRGKbhcCwEzj9I6miLTbmjOBt2PCEjLEdwjMPAz'
-});
-client.get('statuses/user_timeline', function(error, tweets, response){
-  for (var i = 0; i < tweets.length; i++){
-    console.log(tweets[i].text);
-  }
-// end the get function
-});
-
-// end the myTweets function
-
-
-
-}
-
-
-
-function mySpotify(receivedSong) {
-  var theSong = receivedSong ? receivedSong : 'back in black';
-
-  spotify.search({ type: 'track', query: theSong}, function(err, data) {
-    if (err) {
-      return console.log("Error occured: " + err);
+  var client = new Twitter({consumer_key: 'RwqVXzDjzji6RF85KFWrq65kv', consumer_secret: 'TSKB58nbg8nPw79A9gcjv23e1cGU9vVqsLQs11fM997bZRHbd7', access_token_key: '889721993683238912-PALrUB3r09Q6Pgxp0oWe5hIzgnPqTWV', access_token_secret: 'pMlbEqZRGKbhcCwEzj9I6miLTbmjOBt2PCEjLEdwjMPAz'});
+  client.get('statuses/user_timeline', function(error, tweets, response) {
+    for (var i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].text);
     }
-
-    console.log(theSong.artist);
-
+    // end the get function
   });
 
+  // end the myTweets function
+
 }
 
-
-////request
-// request('http://www.google.com', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
+function mySpotify() {
+  spotify.search({
+    type: 'track',
+    query: commandA
+  }, function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return; //from spotify npm docs
+    } else {
+      var songInfo = data.tracks.items[0];
+      var songResult = console.log(songInfo.artists[0].name);
+      console.log(songInfo.name);
+      console.log(songInfo.album.name);
+      console.log(songInfo.preview_url);
+      console.log(songResult);
+    }
+  });
+}
